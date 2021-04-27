93ec0a3ca2d5df17438ab8b69b923716f8de0951
Erik Espinoza <erik_espinoza_mendoza@hotmail.com>
2021-04-27 18:53:26 -0500

    primer commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Piraña.Models;
+
+namespace Piraña.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Servicio()
+        {
+            return View();
+        }
+        public IActionResult Formulario()
+        {
+            TempData["mani"]="Mensaje recibido";
+            return View();
+            
+        }
+        public IActionResult Productos()
+        {
+            return View();
+            
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
